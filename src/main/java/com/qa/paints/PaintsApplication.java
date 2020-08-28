f200a1d2854dde5b8af8206adb3844210e502ca5
ACarley <Alex.carley98@gmail.com>
2020-08-28 14:55:49 +0100

    Initial commit

---
+++ src/main/java/com/qa/paints/PaintsApplication.java
@@ -0,0 +1,13 @@
+package com.qa.paints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintsApplication.class, args);
+    }
+
+}
