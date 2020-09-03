537777d2cf803717e9394cc2748f200589fd8bb2
ACarley <Alex.carley98@gmail.com>
2020-09-03 14:06:54 +0100

    Update with url

--- src/main/java/com/qa/paints/rest/paintsController.java
+++ src/main/java/com/qa/paints/rest/paintsController.java
@@ -6,6 +6,7 @@ import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -37,5 +38,14 @@ public class paintsController {
         return this.paintsService.findPaintByID(id);
     }
 
+    @PutMapping("/updatePaint/{id}")
+    public paints updatePaint(@PathVariable Long id, @RequestBody paints paints){
+        return this.paintsService.updatePaints(id, paints);
+    }
+    @PutMapping("/updatePaintWithParam")
+    public paints updatePaintWithParam(@PathParam("id") Long id, @RequestBody paints paints){
+        //Pass value with ?id=x
+        return this.paintsService.updatePaints(id, paints);
+    }
 
 }
