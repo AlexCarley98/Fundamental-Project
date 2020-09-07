9c9127c05a479376810347ac216ecd87a87839aa
ACarley <Alex.carley98@gmail.com>
2020-09-07 01:28:44 +0100

    Scheme service

--- src/main/java/com/qa/paints/service/schemesService.java
+++ src/main/java/com/qa/paints/service/schemesService.java
@@ -1,38 +1,47 @@
 package com.qa.paints.service;
 
 import com.qa.paints.domain.schemes;
+import com.qa.paints.dto.schemesDTO;
 import com.qa.paints.exceptions.schemeNotFoundException;
 import com.qa.paints.repo.schemesRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class schemesService {
     private final schemesRepository repo;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public schemesService(schemesRepository repo) {
+    public schemesService(schemesRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
     }
-    public List<schemes> findAllSchemes(){
-        return this.repo.findAll();
+    private schemesDTO mapToDTO(schemes schemes){
+        return this.mapper.map(schemes, schemesDTO.class);
+    }
+    public List<schemesDTO> findAllSchemes(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+    public schemesDTO createSchemes(schemes schemes){
+        return this.mapToDTO(this.repo.save(schemes));
     }
 
-    public schemes createSchemes(schemes schemes){
-        return this.repo.save(schemes);
+    public schemesDTO findSchemeByID(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(schemeNotFoundException::new));
     }
 
-    public schemes findSchemeByID(Long id){
-        return this.repo.findById(id).orElseThrow(schemeNotFoundException::new);
-    }
-
-    public schemes updateSchemes(Long id, schemes schemes){
-        schemes update = findSchemeByID(id);
+    public schemesDTO updateSchemes(Long id, schemes schemes){
+        schemes update = this.repo.findById(id).orElseThrow(schemeNotFoundException::new);
         update.setName(schemes.getName());
         update.setFaction(schemes.getFaction());
-        return this.repo.save(update);
+        return this.mapToDTO(this.repo.save(update));
     }
 
     public Boolean deleteSchemesByID(Long id){
